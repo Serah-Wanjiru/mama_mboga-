7de807f7fb6e86189fbfe9c8fe9e30ba3c3cdb00
joycewanjiku <joycemumbiiwanjiku@gmail.com>
2023-05-15 21:14:09 +0300

    assign

--- payment.py
+++ payment.py
@@ -4,10 +4,13 @@ class Payments:
         self.customer=customer
         self.amount=amount
         self.phoneNumber=phoneNumber
+        #updating customers name in the payment 
     def update_customer(self, new_customer):
         self.customer = new_customer
+        #updating amounts in the payment list 
     def update_amount(self, new_amount):
         self.amount = new_amount
+        #updating phone Numbers in the payment list
     def update_phoneNumber(self, new_phoneNumber):
         self.phoneNumber= new_phoneNumber
     def __str__(self):
